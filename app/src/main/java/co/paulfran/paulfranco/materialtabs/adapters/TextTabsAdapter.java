e5fe6fbff34dade96a346c382640dae8096181f8
Paul Franco <paulfranco@me.com>
2018-07-15 12:02:39 -0700

    prepared data source and created a custom adapter

---
+++ app/src/main/java/co/paulfran/paulfranco/materialtabs/adapters/TextTabsAdapter.java
@@ -0,0 +1,38 @@
+package co.paulfran.paulfranco.materialtabs.adapters;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+public class TextTabsAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> fragmentList;
+    private List<String> titleList;
+
+    public TextTabsAdapter(FragmentManager fm, List<Fragment> fragmentList, List<String> titleList) {
+        super(fm);
+        this.fragmentList = fragmentList;
+        this.titleList = titleList;
+    }
+
+    @Override
+    // returns item at position
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    // Returns count
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titleList.get(position);
+    }
+}
